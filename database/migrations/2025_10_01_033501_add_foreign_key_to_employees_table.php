34cb2c1a57dcbc0f75e56455cbc420940eeb128b
yakinikuSSS <nabeelswp@it.student.pens.ac.id>
2025-10-01 10:59:45 +0700

    Menyiapkan Tabel

---
+++ database/migrations/2025_10_01_033501_add_foreign_key_to_employees_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use PHPUnit\Framework\Attributes\After;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('employees', function (Blueprint $table) {
+            $table->unsignedBigInteger('departemen_id')->After('tanggal_masuk');
+            $table->unsignedBigInteger('jabatan_id')->after('departemen_id');
+
+            $table->foreign('departemen_id')
+                ->references('id')
+                ->on('departments')
+                ->onDelete('cascade');
+
+            $table->foreign('jabatan_id')
+                ->references('id')
+                ->on('positions')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('employees', function (Blueprint $table) {
+            $table->dropForeign(['departemen_id']);
+            $table->dropForeign(['jabatan_id']);
+            $table->dropColumn(['departemen_id', 'jabatan_id']);
+        });
+    }
+};
