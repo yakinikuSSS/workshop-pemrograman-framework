fec414a8b927d3580866188c0d4e2e1a6f5e4a96
yakinikuSSS <nabeelswp@it.student.pens.ac.id>
2025-10-02 09:50:15 +0700

    Menambahkan tabel deparments, positions, salaries, attendance

--- database/migrations/2025_10_01_032358_create_positions_table.php
+++ database/migrations/2025_10_01_032358_create_positions_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,8 @@ return new class extends Migration
     {
         Schema::create('positions', function (Blueprint $table) {
             $table->id();
+            $table->string('nama_jabatan', 100);
+            $table->decimal('gaji_pokok', 10, 2);
             $table->timestamps();
         });
     }
--- database/migrations/2025_10_02_024546_create_salaries_table.php
+++ database/migrations/2025_10_02_024546_create_salaries_table.php
@@ -10,15 +10,16 @@ return new class extends Migration {
      */
     public function up(): void
     {
-        Schema::create('attendance', function (Blueprint $table) {
+        Schema::create('salaries', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('karyawan_id');
-            $table->date('tanggal');
-            $table->time('waktu_masuk')->nullable();
-            $table->time('waktu_keluar')->nullable();
-            $table->enum('status_absensi', ['hadir', 'izin', 'sakit', 'alpha']);
+            $table->string('bulan', 10);
+            $table->decimal('gaji_pokok', 10, 2);
+            $table->decimal('tunjangan', 10, 2)->default(0);
+            $table->decimal('potongan', 10, 2)->default(0);
+            $table->decimal('total_gaji', 10, 2);
             $table->timestamps();
-            // Foreign key constraint
+
             $table->foreign('karyawan_id')
                 ->references('id')
                 ->on('employees')
@@ -31,6 +32,6 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('attendance');
+        Schema::dropIfExists('salaries');
     }
 };
