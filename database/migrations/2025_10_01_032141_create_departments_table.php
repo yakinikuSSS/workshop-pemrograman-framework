2d3706ce3e67611e6fa10dfacfa136af0b8e0ebd
yakinikuSSS <nabeelswp@it.student.pens.ac.id>
2025-10-02 09:52:54 +0700

    Merubah migration tabel departments

--- database/migrations/2025_10_01_032141_create_departments_table.php
+++ database/migrations/2025_10_01_032141_create_departments_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('departments', function (Blueprint $table) {
             $table->id();
             $table->string('nama_departemen', 100);
-            $table->decimal('gaji_pokok', 10, 2);
             $table->timestamps();
         });
     }
